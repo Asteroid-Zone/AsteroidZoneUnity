4e1f33985d98972530bdf3bdff45d7da9880c90c
Jarrod Doyle <thejarroddoyle@gmail.com>
2021-02-09 14:38:24 +0000

    Add simple camera switcher

---
+++ Assets/Scripts/PlayGame/CameraManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Camera followCamera, tacticalCamera;
+    // private bool _camsSwitched = false;
+    
+    private void Start()
+    {
+        followCamera.enabled = true;
+        tacticalCamera.enabled = false;
+        // followCamera.gameObject.SetActive(true);
+        // tacticalCamera.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.P)) return;
+        followCamera.enabled = !followCamera.enabled;
+        tacticalCamera.enabled = !tacticalCamera.enabled;
+        // _camsSwitched = !_camsSwitched;
+        // followCamera.gameObject.SetActive(!_camsSwitched);
+        // tacticalCamera.gameObject.SetActive(_camsSwitched);
+
+    }
+}
