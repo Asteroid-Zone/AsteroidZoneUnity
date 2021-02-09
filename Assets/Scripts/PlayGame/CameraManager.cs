fc7db3873cbf1631ccaa155bbea0d224a00a4a14
Jarrod Doyle <thejarroddoyle@gmail.com>
2021-02-09 17:23:48 +0000

    Remove unused code in CameraManager

--- Assets/Scripts/PlayGame/CameraManager.cs
+++ Assets/Scripts/PlayGame/CameraManager.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     public Camera followCamera, tacticalCamera;
-    // private bool _camsSwitched = false;
-    
+
     private void Start()
     {
         followCamera.enabled = true;
         tacticalCamera.enabled = false;
-        // followCamera.gameObject.SetActive(true);
-        // tacticalCamera.gameObject.SetActive(false);
     }
 
     private void Update()
@@ -20,9 +17,5 @@ public class CameraManager : MonoBehaviour
         if (!Input.GetKeyDown(KeyCode.P)) return;
         followCamera.enabled = !followCamera.enabled;
         tacticalCamera.enabled = !tacticalCamera.enabled;
-        // _camsSwitched = !_camsSwitched;
-        // followCamera.gameObject.SetActive(!_camsSwitched);
-        // tacticalCamera.gameObject.SetActive(_camsSwitched);
-
     }
 }
