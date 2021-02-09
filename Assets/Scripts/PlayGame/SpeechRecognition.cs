4a22ac63ed7fbd8bc6823fca5a001194f5d9a420
Oliver Pope <pope10830@gmail.com>
2021-02-09 10:01:54 +0000

    Added speech recognition script

---
+++ Assets/Scripts/PlayGame/SpeechRecognition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeechRecognition : MonoBehaviour
+{
+
+    public Text text;
+    private string myResponse = "...";
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = myResponse;
+    }
+
+    public void GetResponse(string result) {
+        myResponse = result;
+    }
+
+    public void StartSpeechRecognitionInTheBrowser() {
+        Application.ExternalCall("startButtonFromUnity3D");
+    }
+
+}
