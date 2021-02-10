6620400035c1b055427d680e3979ef113f76e140
Jarrod Doyle <thejarroddoyle@gmail.com>
2021-02-10 16:30:38 +0000

    Added comments and removed unneeded includes

--- Assets/Scripts/PlayGame/GridManager.cs
+++ Assets/Scripts/PlayGame/GridManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class GridManager : MonoBehaviour
@@ -13,12 +11,16 @@ public class GridManager : MonoBehaviour
 
     private void Start()
     {
+        // X/Y of the first grid square (top left)
         var startX = -(width / 2) * CellSize + CellSize / 2;
         var startY = (height / 2) * CellSize - CellSize / 2;
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
+                // Instantiate a grid square prefab with `this` as parent
+                // Also sets the canvas text to the sector coords
+                // TODO: See if text setting can be done better
                 var position = new Vector3(startX + (x * CellSize), 0, startY - (y * CellSize));
                 var newGridSquare = Instantiate(gridSquarePrefab, position, Quaternion.identity);
                 newGridSquare.transform.parent = gameObject.transform;
