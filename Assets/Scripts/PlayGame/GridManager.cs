b4bf06e7f1f05c16da1277d04429fafb2092bfb4
Jarrod Doyle <thejarroddoyle@gmail.com>
2021-02-10 15:52:19 +0000

    Basic grid being built at runtime

---
+++ Assets/Scripts/PlayGame/GridManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public GameObject gridSquarePrefab;
+    public int width;
+    public int height;
+
+    private const int CellSize = 10;
+
+    private void Start()
+    {
+        var xBound = (width / 2) * CellSize + CellSize / 2;
+        var yBound = (height / 2) * CellSize + CellSize / 2;
+        for (int y = -yBound + CellSize; y < yBound; y += CellSize)
+        {
+            for (int x = -xBound + CellSize; x < xBound; x += CellSize)
+            {
+                var newGridSquare = Instantiate(gridSquarePrefab, new Vector3(x, 0, y), Quaternion.identity);
+                newGridSquare.transform.parent = gameObject.transform;
+            }
+        }
+    }
+}
