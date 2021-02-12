35bce22952756aa7a2afbab72299b163233ec446
Oliver Pope <pope10830@gmail.com>
2021-02-12 10:12:42 +0000

    Added PlayerData class

---
+++ Assets/Scripts/PlayGame/PlayerData.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public enum Role {
+    StationCommander,
+    MilitaryTactician,
+    MiningCaptain,
+    Researcher
+}
+
+public class PlayerData : MonoBehaviour {
+
+    private Role _role;
+    private int _health;
+    private int _shields;
+    private float _speed;
+
+    private void Start() {
+        _role = Role.StationCommander; // TODO assign roles in the menu
+        _health = 100; // TODO different stats for different roles
+        _shields = 100;
+        _speed = 2.5f;
+    }
+
+}
