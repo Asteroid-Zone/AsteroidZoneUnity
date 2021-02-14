9bc7176170d084b2bd56ccd3a994282a05bb2aab
Milen Georgiev <milendg99@gmail.com>
2021-02-14 15:07:19 +0200

    Make game messages scrollable and scroll to the bottom when a new message is added.

--- Assets/Scripts/PlayGame/EventsManager.cs
+++ Assets/Scripts/PlayGame/EventsManager.cs
@@ -7,19 +7,31 @@ namespace PlayGame
 {
     public class EventsManager : MonoBehaviour
     {
+        public GameObject scrollParent;
+        
         private Text _eventsListText;
+        private ScrollRect _scrollRect;
         private static readonly List<string> EventMessagesQueue = new List<string>();
 
         private void Start()
         {
+            // Get the sub-objects
             _eventsListText = GetComponent<Text>();
+            _scrollRect = scrollParent.GetComponent<ScrollRect>();
         }
 
         private void Update()
         {
+            // Add each message to the events object
             foreach (var eventMessage in EventMessagesQueue)
             {
                 AddEventMessage(eventMessage);
+            }
+
+            // Scroll to the bottom if a new message was added
+            if (EventMessagesQueue.Count > 0)
+            {
+                _scrollRect.normalizedPosition = new Vector2(0, 0);
             }
         
             EventMessagesQueue.Clear();
