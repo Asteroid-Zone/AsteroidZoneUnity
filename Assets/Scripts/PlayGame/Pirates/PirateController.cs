b6437eb277bc758ca30e107dfaae989bb1f53e2d
Milen Georgiev <milendg99@gmail.com>
2021-02-15 23:22:05 +0200

    Pirate now turns at your direction when close to it.

--- Assets/Scripts/PlayGame/Pirates/PirateController.cs
+++ Assets/Scripts/PlayGame/Pirates/PirateController.cs
@@ -1,46 +1,60 @@
-﻿using System.Linq;
-using PlayGame;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
-public class PirateController : MonoBehaviour
+namespace PlayGame.Pirates
 {
-    public float lookRadius = 10f;
+    public class PirateController : MonoBehaviour
+    {
+        public float lookRadius = 10f;
 
-    //public Transform target;
-    private NavMeshAgent _agent;
+        //public Transform target;
+        private NavMeshAgent _agent;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        _agent = GetComponent<NavMeshAgent>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        GameObject closestPlayer = null;
-        float closestPlayerDist = float.MaxValue;
-        PlayerData.Players.ForEach(p =>
+        // Start is called before the first frame update
+        private void Start()
         {
-            var position = p.transform.position;
-            var distance = Vector3.Distance(transform.position, position);
-            if (distance < closestPlayerDist)
-            {
-                closestPlayer = p;
-                closestPlayerDist = distance;
-            }
-        });
-
-        if (closestPlayer != null && closestPlayerDist <= lookRadius)
-        {
-            _agent.SetDestination(closestPlayer.transform.position);
+            _agent = GetComponent<NavMeshAgent>();
         }
-    }
 
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, lookRadius);
+        // Update is called once per frame
+        void Update()
+        {
+            GameObject closestPlayer = null;
+            float closestPlayerDist = float.MaxValue;
+            PlayerData.Players.ForEach(p =>
+            {
+                var position = p.transform.position;
+                var distance = Vector3.Distance(transform.position, position);
+                if (distance < closestPlayerDist)
+                {
+                    closestPlayer = p;
+                    closestPlayerDist = distance;
+                }
+            });
+
+            if (closestPlayer != null && closestPlayerDist <= lookRadius)
+            {
+                _agent.SetDestination(closestPlayer.transform.position);
+
+                if (closestPlayerDist <= _agent.stoppingDistance)
+                {
+                    FaceTarget(closestPlayer.transform);
+                    // TODO: Attack the target
+                }
+            }
+        }
+
+        private void FaceTarget(Transform target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, lookRadius);
+        }
     }
 }
