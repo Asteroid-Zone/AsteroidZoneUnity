f488779ca144038283d7ab01ee7f7c9015b0257a
Oliver Pope <pope10830@gmail.com>
2021-02-15 09:27:01 +0000

    Added files that were not included in the last commit

---
+++ Assets/Scripts/PlayGame/SpaceStation.cs
@@ -0,0 +1,13 @@
+﻿using PlayGame;
+using UnityEngine;
+
+public class SpaceStation : MonoBehaviour
+{
+
+    public GridManager gridManager;
+    
+    void Start() {
+        transform.position = gridManager.GetGridCentre();
+    }
+
+}
