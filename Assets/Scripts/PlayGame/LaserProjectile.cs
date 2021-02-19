9be0c8d07a34831273ef7ac32d0e6c788e0dc6c3
Oliver Pope <pope10830@gmail.com>
2021-02-19 14:14:09 +0000

    Destroy laser on collision

---
+++ Assets/Scripts/PlayGame/LaserProjectile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace PlayGame {
+    public class LaserProjectile : MonoBehaviour {
+
+        private void OnCollisionEnter(Collision collision) {
+            if (collision.gameObject.CompareTag("Player")) return;
+            if (collision.gameObject.CompareTag("Pirate")) {
+                Destroy(collision.gameObject); // TODO damage pirate instead of deleting it
+            }
+
+            Destroy(gameObject);
+        }
+        
+    }
+}
