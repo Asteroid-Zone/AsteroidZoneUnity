78a85652c2362ace827533c44b6e6bc343adeb20
Oliver Pope <pope10830@gmail.com>
2021-02-19 14:20:07 +0000

    Mine asteroid without recieving resources if asteroid is shot

--- Assets/Scripts/PlayGame/LaserProjectile.cs
+++ Assets/Scripts/PlayGame/LaserProjectile.cs
@@ -5,8 +5,10 @@ namespace PlayGame {
 
         private void OnCollisionEnter(Collision collision) {
             if (collision.gameObject.CompareTag("Player")) return;
-            if (collision.gameObject.CompareTag("Pirate")) {
-                Destroy(collision.gameObject); // TODO damage pirate instead of deleting it
+            if (collision.gameObject.CompareTag("Pirate")) Destroy(collision.gameObject); // TODO damage pirate instead of deleting it
+            if (collision.gameObject.CompareTag("Asteroid")) {
+                Asteroid asteroid = collision.gameObject.GetComponent<Asteroid>();
+                asteroid.MineAsteroid();
             }
 
             Destroy(gameObject);
