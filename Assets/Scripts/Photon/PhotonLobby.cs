ef39ba6938197b003013aaf30aa455a28a22679e
David Nzewi <cn17834@bristol.ac.uk>
2021-02-27 16:58:04 +0000

    Save for deleting playerShip prefab from main menu

---
+++ Assets/Scripts/Photon/PhotonLobby.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace PhotonClass
+{
+    public class PhotonLobby : MonoBehaviourPunCallbacks 
+    {
+        [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+        [SerializeField]
+        private byte maxPlayersPerRoom = 4;
+
+        [Tooltip("The UI Panel with Main Menu Options")]
+        [SerializeField]
+        private GameObject controlPanel;
+        [Tooltip("The UI Label to inform the user that the connection is in progress")]
+        [SerializeField]
+        private GameObject progressLabel;
+
+        public static PhotonLobby lobby;
+
+
+        /// This client's version number. Users are separated from each other by gameVersion.
+        string gameVersion = "1";
+
+        /// Keep track of the current process. Since connection is asynchronous and is based on several callbacks from Photon,
+        /// we need to keep track of this to properly adjust the behavior when we receive call back by Photon.
+        /// This is used for the OnConnectedToMaster() callback when a user wants to leave the game.
+        bool isConnecting;
+
+        /// MonoBehaviour methods called on GameObject by Unity during early initialization phase.
+        
+        private void Awake()
+        {
+            lobby = this;
+        }
+
+        void Start()
+        {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
+        }
+
+        public static PhotonLobby getInstance()
+        {
+            return lobby;
+        }
+
+        /// Start the connection process.
+        /// - If already connected, we attempt joining a random room
+        /// - if not yet connected, Connect this application instance to Photon Cloud Network
+        public void Connect()
+        {
+            progressLabel.SetActive(true);
+            controlPanel.SetActive(false);
+
+            if (PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                // keep track of the will to join a room, because when we come back from the game we will get a callback that we are connected, so we need to know what to do then
+                isConnecting = PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = gameVersion;
+            }
+        }
+
+        //Methods overriding MonoBehaviourPunCallbacks Callbacks
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("Asteroid Zone/MainMenuFunction: OnConnectedToMaster() was called by PUN");
+            // we don't want to do anything if we are not attempting to join a room.
+            // this case where isConnecting is false is typically when you lost or quit the game, when this level is loaded, OnConnectedToMaster will be called, in that case
+            // we don't want to do anything.
+            if (isConnecting)
+            {
+                // The first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnJoinRandomFailed()
+                PhotonNetwork.JoinRandomRoom();
+                isConnecting = false;
+            }
+
+        }
+
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
+            isConnecting = false;
+            Debug.LogWarningFormat("Asteroid Zone/MainMenuFunction: OnDisconnected() was called by PUN with reason {0}", cause);
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("Asteroid Zone/MainMenuFunction: OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Photon/PhotonRoom.cs
@@ -0,0 +1,94 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PhotonClass
+{
+    public class PhotonRoom : MonoBehaviourPunCallbacks, IInRoomCallbacks
+    {
+        public static PhotonRoom room;
+        private PhotonView PV;
+        
+        public int multiplayerScene;
+        private int currentScene;
+
+        void Awake()
+        {
+            //Set up Singleton
+            if (PhotonRoom.room == null)
+            {
+                PhotonRoom.room = this;
+            }
+            else
+            {
+                if (PhotonRoom.room != this)
+                {
+                    Destroy(PhotonRoom.room.gameObject);
+                    PhotonRoom.room = this;
+                }
+            }
+            DontDestroyOnLoad(this.gameObject);
+            PV = GetComponent<PhotonView>();
+        }
+
+        public override void OnEnable()
+        {
+            //subscribe to functions
+            base.OnEnable();
+            PhotonNetwork.AddCallbackTarget(this);
+            SceneManager.sceneLoaded += OnSceneFinishedLoading;
+        }
+
+        public override void OnDisable()
+        {
+            //subscribe to functions
+            base.OnDisable();
+            PhotonNetwork.RemoveCallbackTarget(this);
+            SceneManager.sceneLoaded -= OnSceneFinishedLoading;
+        }
+
+        public override void OnJoinedRoom()
+        {
+            //sets player data when we join the room
+            base.OnJoinedRoom();
+            Debug.Log("Asteroid Zone/MainMenuFunction: OnJoinedRoom() called by PUN. Now this client is in a room.");
+            if (!PhotonNetwork.IsMasterClient) return;
+            StartGame();
+        }
+
+        void StartGame()
+        {
+            Debug.Log("Loading Level");
+            PhotonNetwork.LoadLevel(multiplayerScene);
+        }
+
+        void OnSceneFinishedLoading(Scene scene, LoadSceneMode mode)
+        {
+            currentScene = scene.buildIndex;
+            if (currentScene == multiplayerScene)
+            {
+                CreatePlayer();
+            }
+        }
+
+        private void CreatePlayer()
+        {
+
+            PhotonNetwork.Instantiate("PlayerShip", new Vector3(5f, 0f, 5f), Quaternion.identity, 0);
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
