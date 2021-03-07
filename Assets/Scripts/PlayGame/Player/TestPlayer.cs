151f1025d0fc418f3a90c845bb9d9e59043ec6a3
Milen Georgiev <milendg99@gmail.com>
2021-03-07 17:28:29 +0200

    Test PlayerShip is now removed if game is in multiplayer mode.

---
+++ Assets/Scripts/PlayGame/Player/TestPlayer.cs
@@ -0,0 +1,17 @@
+﻿using Statics;
+using UnityEngine;
+
+namespace PlayGame.Player
+{
+    public class TestPlayer : MonoBehaviour
+    {
+        // Enable the test player ship if Debug mode.
+        private void Start()
+        {
+            if (!DebugSettings.Debug)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
