d4d27516d91844c089fb188d319ef0e5f5b05cea
Milen Georgiev <milendg99@gmail.com>
2021-03-07 21:04:08 +0200

    Merged changes from remote

---
+++ Assets/Scripts/PlayGame/UI/TipsFunctions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace PlayGame.UI
+{
+    public class TipsFunctions : MonoBehaviour
+    {
+        public GameObject tipsPanel;
+
+        public void ShowTipsPanel()
+        {
+            tipsPanel.SetActive(true);
+        }
+
+        public void HideTipsPanel()
+        {
+            tipsPanel.SetActive(false);
+        }
+    }
+}
