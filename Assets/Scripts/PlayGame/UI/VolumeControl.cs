2059e5d538ad8523d6acb19a8aa8075cc5663f69
Milen Georgiev <milendg99@gmail.com>
2021-03-08 17:39:56 +0200

    Volume changed via volume slider

---
+++ Assets/Scripts/PlayGame/UI/VolumeControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace PlayGame.UI
+{
+    public class VolumeControl : MonoBehaviour
+    {
+        public GameObject backgroundAudio;
+        public Slider volumeSlider;
+        private AudioSource _audioSource;
+        
+        private void Start()
+        {
+            _audioSource = backgroundAudio.GetComponent<AudioSource>();
+            volumeSlider.value = _audioSource.volume;
+            volumeSlider.onValueChanged.AddListener(delegate { ChangeVolume(); });
+        }
+
+        private void ChangeVolume()
+        {
+            _audioSource.volume = volumeSlider.value;
+        }
+    }
+}
