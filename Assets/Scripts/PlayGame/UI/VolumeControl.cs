94ebd03faca9d8929275f9186b1ac2bb8e3c7375
Milen Georgiev <milendg99@gmail.com>
2021-03-08 17:44:22 +0200

    Volume saved in player prefs

--- Assets/Scripts/PlayGame/UI/VolumeControl.cs
+++ Assets/Scripts/PlayGame/UI/VolumeControl.cs
@@ -5,6 +5,9 @@ namespace PlayGame.UI
 {
     public class VolumeControl : MonoBehaviour
     {
+        // Store the VolumePref Key to avoid typos
+        private const string VolumePrefKey = "VolumePreference";
+        
         public GameObject backgroundAudio;
         public Slider volumeSlider;
         private AudioSource _audioSource;
@@ -12,6 +15,14 @@ namespace PlayGame.UI
         private void Start()
         {
             _audioSource = backgroundAudio.GetComponent<AudioSource>();
+
+            // Get the volume from the prefs
+            if (PlayerPrefs.HasKey(VolumePrefKey))
+            {
+                _audioSource.volume = PlayerPrefs.GetFloat(VolumePrefKey);
+            }
+            
+            // Set the volume and register the listener to the slider changed event
             volumeSlider.value = _audioSource.volume;
             volumeSlider.onValueChanged.AddListener(delegate { ChangeVolume(); });
         }
@@ -19,6 +30,9 @@ namespace PlayGame.UI
         private void ChangeVolume()
         {
             _audioSource.volume = volumeSlider.value;
+            
+            // Save the volume in the prefs
+            PlayerPrefs.SetFloat(VolumePrefKey, volumeSlider.value);
         }
     }
 }
