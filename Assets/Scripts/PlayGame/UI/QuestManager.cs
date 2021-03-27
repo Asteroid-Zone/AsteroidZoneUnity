99c1f8584f2cf0c9621a6f6780b890983e142e09
Omer <ok17659@my.bristol.ac.uk>
2021-03-27 11:08:17 +0000

    Display current quest on UI

---
+++ Assets/Scripts/PlayGame/UI/QuestManager.cs
@@ -0,0 +1,47 @@
+﻿using Photon.GameControllers;
+using PlayGame.Player;
+using Statics;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PlayGame.UI
+{
+    public class QuestManager : MonoBehaviour {
+    
+        public GameObject player;
+
+        private PlayerData _playerData;
+        
+        private Text _text;
+
+        private void Start() {
+            player = !DebugSettings.Debug ? PhotonPlayer.Instance.myAvatar : TestPlayer.GetPlayerShip();
+            _text = GetComponent<Text>();
+            _playerData = player.GetComponent<PlayerData>();
+        }
+
+        private void Update()
+        {
+            string questStr = questToString(_playerData.currentQuest);
+            _text.text = questStr;
+        }
+
+        private string questToString(QuestType quest)
+        {
+            switch (quest)
+            {   
+                case QuestType.DefendStation:
+                    return "Defend the station.";
+                case QuestType.MineAsteroids:
+                    return "Mine asteroids.";
+                case QuestType.PirateWarning:
+                    return "Deal with pirates in the area.";
+                case QuestType.ReturnToStation:
+                    return "Return to the space station.";
+            }
+
+            return "";
+        }
+        
+    }
+}
