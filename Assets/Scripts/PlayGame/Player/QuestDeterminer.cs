82e013a2ed3389d89f9fb2724208c82da2339334
Omer <ok17659@my.bristol.ac.uk>
2021-03-31 20:22:38 +0100

    Added quest to defend space station if being attacked

--- Assets/Scripts/PlayGame/Player/QuestDeterminer.cs
+++ Assets/Scripts/PlayGame/Player/QuestDeterminer.cs
@@ -2,54 +2,63 @@
 using System.Collections.Generic;
 using Photon.GameControllers;
 using PlayGame.Player;
+using PlayGame.SpaceStation;
 using Statics;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class QuestDeterminer : MonoBehaviour
 {
 
     private PlayerData _playerData;
-    private bool stationDamaged;
+    private bool _stationDamaged;
+    private UnityEvent _stationAttacked;
 
     // Start is called before the first frame update
     void Start()
     {
         _playerData = GetComponent<PlayerData>();
-        stationDamaged = false;
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        StartCoroutine(calculateQuest());
+        _stationDamaged = false;
+        // Can't find a better way of doing this
+        SpaceStation spaceStation = GameObject.Find("SpaceStation").GetComponent<SpaceStation>();
+        _stationAttacked = spaceStation.stationAttacked;
+        _stationAttacked.AddListener(OnStationDamaged);
+        StartCoroutine(DetermineQuest());
     }
     
-    // If station is damaged, this overrides the current event
-    public void stationDamagedToggle()
+    public void OnStationDamaged()
     {
-        Debug.Log("Station damaged!");
-        stationDamaged = true;
+        _stationDamaged = true;
     }
-    private IEnumerator calculateQuest()
+    
+
+    private IEnumerator DetermineQuest()
     {
-        if (stationDamaged)
+        while (true)
         {
-            _playerData.SetQuest(QuestType.DefendStation);
-            yield return new WaitForSeconds(4);
-            stationDamaged = false;
+            if (_stationDamaged)
+            {
+                _playerData.SetQuest(QuestType.DefendStation);
+                yield return new WaitForSeconds(4);
+                _stationDamaged = false;
+                // TODO: Add flag for station commander saying pirates around 
+            }
+
+            else
+            {
+                if (_playerData.GetResources() > 75)
+                {
+                    _playerData.SetQuest(QuestType.ReturnToStation);
+                }
+                else
+                {
+                    _playerData.SetQuest(QuestType.MineAsteroids);
+                }
+            }
+
+            yield return null;
         }
-        // TODO: Add flag for station commander saying pirates around 
-            
-        if (_playerData.GetResources() > 75)
-        {
-            _playerData.SetQuest(QuestType.ReturnToStation);
-        }
-        else
-        {
-            _playerData.SetQuest(QuestType.MineAsteroids);
-        }
-            
+
     }
 }
