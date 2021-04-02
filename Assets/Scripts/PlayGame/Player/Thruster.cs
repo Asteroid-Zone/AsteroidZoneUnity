4438c648c9a2ff29870f7ba810733fea03057ace
Milen Georgiev <milendg99@gmail.com>
2021-04-02 16:24:30 +0300

    Fixed error preventing code from being compiled and cleaned code style a bit

--- Assets/Scripts/PlayGame/Player/Thruster.cs
+++ Assets/Scripts/PlayGame/Player/Thruster.cs
@@ -1,36 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.UIElements;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Thruster : MonoBehaviour
+namespace PlayGame.Player
 {
-    private Transform _shipTransform;
-    private ParticleSystem _thrusterParticles;
-    private Vector3 _prevPosition;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Thruster : MonoBehaviour
     {
-        _shipTransform = transform.parent.parent.parent;
-        _thrusterParticles = GetComponent<ParticleSystem>();
-        _prevPosition = _shipTransform.position;
-    }
+        private Transform _shipTransform;
+        private ParticleSystem _thrusterParticles;
+        private Vector3 _prevPosition;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 currentPosition = _shipTransform.position;
-        // Probably a better way to do this
-        //float speed = (currentPosition - _prevPosition).sqrMagnitude / Time.deltaTime;
-        if (currentPosition != _prevPosition)
+        // Start is called before the first frame update
+        private void Start()
         {
-            _thrusterParticles.Play();
-            _prevPosition = currentPosition;
+            _shipTransform = transform.parent.parent.parent;
+            _thrusterParticles = GetComponent<ParticleSystem>();
+            _prevPosition = _shipTransform.position;
         }
-        else
+
+        // Update is called once per frame
+        private void Update()
         {
-            _thrusterParticles.Stop();
+            Vector3 currentPosition = _shipTransform.position;
+            // Probably a better way to do this
+            //float speed = (currentPosition - _prevPosition).sqrMagnitude / Time.deltaTime;
+            if (currentPosition != _prevPosition)
+            {
+                _thrusterParticles.Play();
+                _prevPosition = currentPosition;
+            }
+            else
+            {
+                _thrusterParticles.Stop();
+            }
         }
     }
 }
