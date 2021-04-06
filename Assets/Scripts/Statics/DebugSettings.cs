f1422f9bcfbb5f1439e9589646f68db483df9c1e
Milen Georgiev <milendg99@gmail.com>
2021-04-06 17:41:19 +0300

    Converted constants in DebugSettings to readonly statics as code was grayed-out when using the constants in if statements. Note: constants are faster than readonly static variables, but the difference would not be a problem

--- Assets/Scripts/Statics/DebugSettings.cs
+++ Assets/Scripts/Statics/DebugSettings.cs
@@ -1,11 +1,15 @@
 ﻿namespace Statics
 {
+    // ReSharper disable always ConvertToConstant.Global
     public static class DebugSettings {
-        public const bool Debug = false; // true disables photon, allows running in unity editor
+        // Readonly variables are used instead of constants, because when using constants, the code grays out the if statements in which the variables are used
+        // as they are always either true or false
+        
+        public static readonly bool Debug = false; // true disables photon, allows running in unity editor
         public static bool DebugKeys = false; // disables/enables keyboard controls
         public static bool ArrowKeys = false; // disables/enables arrow key controls
 
-        public const bool SpawnPirates = true; // enables/disables pirate spawning
-        public const bool InfiniteMiningRange = false;
+        public static readonly bool SpawnPirates = true; // enables/disables pirate spawning
+        public static readonly bool InfiniteMiningRange = false;
     }
 }
