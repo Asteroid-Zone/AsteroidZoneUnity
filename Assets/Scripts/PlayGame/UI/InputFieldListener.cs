07fb5bd6cffb7f8d231da7a588b04f556fe43bc9
Oliver Pope <pope10830@gmail.com>
2021-04-07 12:05:10 +0100

    Player Constants

--- Assets/Scripts/PlayGame/UI/InputFieldListener.cs
+++ Assets/Scripts/PlayGame/UI/InputFieldListener.cs
@@ -29,6 +29,42 @@ namespace PlayGame.UI {
                     if (initial) _inputField.text = GameConstants.PlayerRotateSpeed.ToString();
                     else GameConstants.PlayerRotateSpeed = float.Parse(value);
                     break;
+                case "PlayerMiningRateInput":
+                    if (initial) _inputField.text = GameConstants.PlayerMiningRate.ToString();
+                    else GameConstants.PlayerMiningRate = int.Parse(value);
+                    break;
+                case "PlayerMiningRangeInput":
+                    if (initial) _inputField.text = GameConstants.PlayerMiningRange.ToString();
+                    else GameConstants.PlayerMiningRange = int.Parse(value);
+                    break;
+                case "PlayerMiningDelayInput":
+                    if (initial) _inputField.text = GameConstants.PlayerMiningDelay.ToString();
+                    else GameConstants.PlayerMiningDelay = int.Parse(value);
+                    break;
+                case "PlayerShotDelayInput":
+                    if (initial) _inputField.text = GameConstants.PlayerShotDelay.ToString();
+                    else GameConstants.PlayerShotDelay = int.Parse(value);
+                    break;
+                case "PlayerLaserSpeedInput":
+                    if (initial) _inputField.text = GameConstants.PlayerLaserSpeed.ToString();
+                    else GameConstants.PlayerLaserSpeed = int.Parse(value);
+                    break;
+                case "PlayerLaserDamageInput":
+                    if (initial) _inputField.text = GameConstants.PlayerLaserDamage.ToString();
+                    else GameConstants.PlayerLaserDamage = int.Parse(value);
+                    break;
+                case "PlayerLaserDamageRangeInput":
+                    if (initial) _inputField.text = GameConstants.PlayerLaserDamageRange.ToString();
+                    else GameConstants.PlayerLaserDamageRange = int.Parse(value);
+                    break;
+                case "PlayerLaserMiningRateInput":
+                    if (initial) _inputField.text = GameConstants.PlayerLaserMiningRate.ToString();
+                    else GameConstants.PlayerLaserMiningRate = int.Parse(value);
+                    break;
+                case "PlayerLaserRangeInput":
+                    if (initial) _inputField.text = GameConstants.PlayerLaserRange.ToString();
+                    else GameConstants.PlayerLaserRange = int.Parse(value);
+                    break;
             }
         }
 
