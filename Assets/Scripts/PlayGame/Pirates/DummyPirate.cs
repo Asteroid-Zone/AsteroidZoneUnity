2a041cc250b987d47066a376a28564263a3e2e18
Milen Georgiev <milendg99@gmail.com>
2021-04-27 16:25:23 +0300

    Added dummy pirate functionality for a disabled pirate that can be made to not die for testing purposes

---
+++ Assets/Scripts/PlayGame/Pirates/DummyPirate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace PlayGame.Pirates
+{
+    public class DummyPirate : MonoBehaviour
+    {
+        private void Start()
+        {
+            // The Die() method in PirateData should also be commented out in order for the dummy pirate to not die
+            transform.parent = PirateSpawner.GetInstance().transform;
+        }
+    }
+}
