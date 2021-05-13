ef906b2a6a2f0139e0d88aae1752b953fe88650a
Legitlolly <jmatthews17754@gmail.com>
2021-05-14 00:50:07 +0100

    needs fix

---
+++ Assets/Tests/FirstAttemptAtTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using Ping = PlayGame.Pings.Ping;
+
+namespace Tests
+{
+    public class FirstAttemptAtTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CreateAPing()
+        {
+          new Ping('A', 1, 1);
+          Assert.AreEqual(1, GetGridCoord());
+            // Use the Assert class to test conditions
+        }
+
+
+    }
+}
