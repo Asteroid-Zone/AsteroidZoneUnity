08fff958daeb4debd0bfdb6d93cf216ddd61f961
Jarrod Doyle <thejarroddoyle@gmail.com>
2021-05-16 14:21:00 +0100

    End game cutscene plays different music depending on victory status

---
+++ Assets/Scripts/EndGame/PostGameMusicSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using PlayGame;
+using PlayGame.Stats;
+using UnityEngine;
+
+namespace EndGame
+{
+    public class PostGameMusicSwitcher : MonoBehaviour
+    {
+        public AudioClip victoryMusic;
+        public AudioClip defeatMusic;
+        
+        private AudioSource _audioSource;
+        
+        private void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _audioSource.clip = StatsManager.GameStats.gameOverType == GameManager.GameOverType.Victory ? victoryMusic : defeatMusic;
+            _audioSource.Play();
+        }
+    }
+}
