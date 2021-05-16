db64d233f91b266ceca68c985c913c61f7ec6a8d
Milen Georgiev <milendg99@gmail.com>
2021-05-16 16:12:28 +0300

    Fixed tutorial voice control

--- Assets/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -16,6 +16,28 @@ using Ping = PlayGame.Pings.Ping;
 
 namespace Tutorial {
     public class TutorialManager : MonoBehaviour {
+        
+        // Make the class a singleton
+        #region Singleton
+        private static TutorialManager _instance;
+
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instance = this;
+            }
+        }
+
+        public static TutorialManager GetInstance()
+        {
+            return _instance;
+        }
+        #endregion
 
         public Text inputText;
         public Text subtitlesText;
